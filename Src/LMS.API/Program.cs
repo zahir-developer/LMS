4899be59841e303953ddc1f7a40673c2b0a8e9c2
iZahir.Developer <izahir.developer@gmail.com>
2024-05-20 19:07:16 +0530

    Program.cs - IServiceCollection extension method added

--- Src/LMS.API/Program.cs
+++ Src/LMS.API/Program.cs
@@ -1,105 +1,27 @@
-using LMS.Infrastructure.Repositories;
-using LMS.Infrastructure.Database;
-using LMS.Application.Interfaces;
-using LMS.Application.Services;
-using LMS.Application.Mappings;
+
 using LMS.Application.ServiceMappings;
 using LMS.Application.IServiceMappings;
 using LMS.Application.Interfaces.IServices;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.OpenApi.Models;
+using LMS.API.Extensions;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
- 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "MyAllowSpecificOrigins",
-    policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
-    });
-});
-
 builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddIdentityServices(builder.Configuration);
 
 // builder.Services.AddDbContext<LMSDbContext>(options => 
 // options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
-builder.Services.AddDbContext<LMSDbContext>(options => 
-options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
-
-//AutoMapper Configuration
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-//Generic Repository
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
-//Generic Services
-builder.Services.AddScoped(typeof(IReadServiceAsync<,>), typeof(ReadServiceAsync<,>));
-
-// Services
-builder.Services.AddScoped(typeof(IUserService), typeof(UserService));
-builder.Services.AddScoped<IAuthTokenService, AuthTokenService>();
-
-#region Authentication JWT Token
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(option =>
-{
-    option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(builder.Configuration["JwtTokenKey"])),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-
-#endregion
-
-#region Swagger Authorization
-builder.Services.AddSwaggerGen(option =>
-{
-    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
-    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter a valid token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
-                }
-            },
-            new string[]{}
-        }
-    });
-});
-
-#endregion
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddSwaggerConfigration();
 
 var app = builder.Build();
 
