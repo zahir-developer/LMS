d6b834ff51b4003e9817583b2372c159eecac23d
iZahir.Developer <izahir.developer@gmail.com>
2024-05-22 15:08:40 +0530

    API - RoleId related changes

--- Src/LMS.Application/DTOs/AddUserDto.cs
+++ Src/LMS.Application/DTOs/AddUserDto.cs
@@ -12,5 +12,6 @@ namespace LMS.Application.DTOs
         public string LastName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public int RoleId { get; set; }
     }
 }
