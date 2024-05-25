7a63543be09d535172ca738e014e4c76a462cc12
iZahir.Developer <izahir.developer@gmail.com>
2024-05-26 02:20:03 +0530

    Fluent Validation using FluentValidation.AspNetCore. Implemented for LeaveStatusUpdateDto

---
+++ Src/LMS.API/Extensions/FluentValidationServicesExtension.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using LMS.Application.DTOs;
+using LMS.API.FluentValidators;
+
+namespace LMS.API.Extensions;
+
+public static class FluentValidationServicesExtension
+{
+    public static IServiceCollection AddFluentValidationServicesExtension(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<LeaveStatusUpdateDto>, LeaveStatusUpdateValidator>();
+        services.AddFluentValidationAutoValidation()
+        .AddFluentValidationClientsideAdapters()
+        .AddValidatorsFromAssemblyContaining<LeaveStatusUpdateValidator>();
+        
+
+        // Load using a type reference rather than the generic.
+        //services.AddValidatorsFromAssemblyContaining(typeof(LeaveStatusUpdateValidator));
+            
+        //services.AddValidatorsFromAssemblyContaining<LeaveStatusUpdateValidator>(ServiceLifetime.Transient);
+        return services;
+
+    }
+}
